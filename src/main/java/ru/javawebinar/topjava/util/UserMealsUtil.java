af1628f8f2539d63c5376795d2db80951a271c18
Arcanum <arkgah@gmail.com>
2016-06-01 00:02:00 +0300

    HW0-optional [~]Refactoring was performed. An aux static method for a Map.Entry was removed

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -7,11 +7,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- * GKislin
+ * Arcanum
  * 31.05.2015.
  */
 public class UserMealsUtil {
@@ -35,35 +37,34 @@ public class UserMealsUtil {
      * превышает ли сумма калорий за весь день параметра метода caloriesPerDay
      * Т.е UserMealWithExceed - это запись одной еды, но поле exceeded будет одинаково для всех записей за этот день
      *
-     * @param mealList
-     * @param startTime
-     * @param endTime
-     * @param caloriesPerDay
-     * @return
+     * @param mealList a list of UserMeal to convert
+     * @param startTime the start time of a period to filter
+     * @param endTime the end time of a period to filter
+     * @param caloriesPerDay calories per day to calculate the exceeded flag
+     * @return the list of UserMealWithExceed, filtered by time
      */
     public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
 
-        // Count the calories per a day basis
-        Map<LocalDate, Integer> mapCaloriesByDate = mealList
+        // Count the calories per a day basis, then derive the exceeded flag
+        Map<LocalDate, Boolean> mapCaloriesByDate = mealList
                 .stream()
-                .map(e -> pair(e.getDateTime().toLocalDate(), e.getCalories()))
                 .collect(Collectors.groupingBy(
-                        java.util.Map.Entry::getKey,
-                        Collectors.summingInt(java.util.Map.Entry::getValue)));
-
+                        userMeal -> userMeal.getDateTime().toLocalDate(),
+                        Collectors.collectingAndThen(
+                                Collectors.summingInt(UserMeal::getCalories),
+                                sum -> sum > caloriesPerDay ? true : false
+                        )
+                        )
+                );
+        // Then filter by the time intervals with following setting of he flag
         List<UserMealWithExceed> result = mealList
                 .stream()
                 .filter(e -> TimeUtil.isBetween(e.getDateTime().toLocalTime(), startTime, endTime))
                 .map(e -> new UserMealWithExceed(e.getDateTime(), e.getDescription(), e.getCalories(),
-                        mapCaloriesByDate.get(e.getDateTime().toLocalDate()) > caloriesPerDay ? true : false))
+                        mapCaloriesByDate.get(e.getDateTime().toLocalDate())))
                 .collect(Collectors.toList());
 
-    return result;
-}
-
-    private static <K, V> AbstractMap.Entry<K, V> pair(K k, V v) {
-        return new AbstractMap.SimpleImmutableEntry<K, V>(k, v);
+        return result;
     }
-
 
 }
