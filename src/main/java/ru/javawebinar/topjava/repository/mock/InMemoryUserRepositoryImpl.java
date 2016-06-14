11891db7ae72b62454da36109bc966a1578a4a71
Arcanum <arkgah@gmail.com>
2016-06-14 12:03:44 +0300

    [+] In memory user repository was implemented

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * Arcanum
+ * 14.06.2016
+ */
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        return repository.remove(id) != null;
+    }
+
+    @Override
+    public User save(User user) {
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return repository.values().stream()
+                .sorted((u1, u2) -> u1.getName().compareTo(u2.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return repository.values().stream()
+                .filter(user -> user.getEmail().equals(email)).findFirst().orElse(null);
+    }
+}
