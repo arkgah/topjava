a558038f1d10a87b814a037a80bc574d716abedb
Arcanum <arkgah@gmail.com>
2016-06-15 00:30:27 +0300

    [+] HW02 done (alfa)

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserMealRepositoryImpl.java
@@ -2,11 +2,16 @@ package ru.javawebinar.topjava.repository.mock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.LoggedUser;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
+import ru.javawebinar.topjava.to.UserMealWithExceed;
 import ru.javawebinar.topjava.util.UserMealsUtil;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,9 +19,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
- * GKislin
- * 15.09.2015.
+ * Arcanum
+ * 14.06.2016.
  */
+@Repository
 public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
     private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserMealRepositoryImpl.class);
     public static final String NOT_AUTHORIZED = "Not authorized";
@@ -31,7 +37,7 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
 
     @Override
     public UserMeal save(int userId, UserMeal userMeal) {
-        if (!isLoggedUser(userId)) {
+        if (!isOpertationAllowed(userId, userMeal)) {
             return null;
         }
 
@@ -43,16 +49,16 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
     }
 
     @Override
-    public void delete(int userId, int mealId) {
-        if (!isLoggedUser(userId)) {
-            return;
+    public boolean delete(int userId, int mealId) {
+        if (!isOpertationAllowed(userId, repository.get(mealId))) {
+            return false;
         }
-        repository.remove(mealId);
+        return repository.remove(mealId) != null;
     }
 
     @Override
     public UserMeal get(int userId, int mealId) {
-        if (!isLoggedUser(userId)) {
+        if (!isOpertationAllowed(userId, repository.get(mealId))) {
             return null;
         }
         return repository.get(mealId);
@@ -63,13 +69,35 @@ public class InMemoryUserMealRepositoryImpl implements UserMealRepository {
         if (!isLoggedUser(userId)) {
             return null;
         }
-        return repository.values().stream()
+        return repository.values().stream().filter(meal -> meal.getUserId().equals(userId))
                 .sorted((meal1, meal2) -> meal2.getDateTime().compareTo(meal1.getDateTime()))
                 .collect(Collectors.toList());
     }
 
+    @Override
+    public Collection<UserMeal> getFiltered(int userId, LocalDate beginDate, LocalTime beginTime, LocalDate endDate,
+                                            LocalTime endTime) {
+        if (!isLoggedUser(userId)) {
+            return null;
+        }
+        return UserMealsUtil.getFilteredByDateTime(repository.values().stream()
+                .filter(meal -> meal.getUserId().equals(userId))
+                .collect(Collectors.toList()), beginDate, beginTime, endDate, endTime);
+    }
+
     private boolean isLoggedUser(int userId) {
         return LoggedUser.id() == userId;
+    }
+
+    private boolean isLoggedUserMeal(UserMeal meal) {
+        if (meal == null) {
+            return false;
+        }
+        return LoggedUser.id() == meal.getUserId();
+    }
+
+    private boolean isOpertationAllowed(int userId, UserMeal userMeal) {
+        return isLoggedUser(userId) && isLoggedUserMeal(userMeal);
     }
 
     private UserMeal init(UserMeal userMeal) {
